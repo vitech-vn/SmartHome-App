a22b294db3a71dd7a7b0ff8eca699b9b9a4c5242
Vi <lamvi2692k6@gmail.com>
2026-06-13 13:11:47 +0700

    Update Device.cpp

--- SmartHomeProject/Backend/Device.cpp
+++ SmartHomeProject/Backend/Device.cpp
@@ -1,2 +1,176 @@
 #include "Device.h"
-Device::Device(){}
+
+
+
+Device::Device(
+    std::string id,
+    std::string name,
+    std::string roomName,
+    std::string type,
+    double power
+
+)
+
+{
+
+this->id=id;
+
+this->name=name;
+
+this->roomName=roomName;
+
+this->type=type;
+
+
+currentPower=power;
+
+
+isOn=false;
+
+
+totalConsumption=0;
+
+
+extraParam=0;
+
+
+runningHours=0;
+
+
+currentSessionHours=0;
+
+
+}
+
+
+
+Device::~Device()
+{
+
+}
+
+
+
+void Device::turnOn()
+
+{
+
+isOn=true;
+
+}
+
+
+
+void Device::turnOff()
+
+{
+
+isOn=false;
+
+currentSessionHours=0;
+
+}
+
+
+
+void Device::updateEnergy(double deltaHours)
+
+{
+
+
+if(isOn)
+
+{
+
+    double kwh =
+    currentPower * deltaHours /1000.0;
+
+
+
+    totalConsumption += kwh;
+
+
+    runningHours += deltaHours;
+
+
+    currentSessionHours += deltaHours;
+
+}
+
+
+}
+
+
+
+void Device::updateParam(int value)
+
+{
+
+extraParam=value;
+
+}
+
+
+
+DeviceInfo Device::getInfo() const
+
+{
+
+DeviceInfo info;
+
+
+info.id=id;
+
+info.name=name;
+
+info.roomName=roomName;
+
+info.type=type;
+
+
+info.isOn=isOn;
+
+
+info.currentPower=currentPower;
+
+
+info.totalConsumption=
+totalConsumption;
+
+
+info.extraParam=
+extraParam;
+
+
+info.runningHours=
+runningHours;
+
+
+info.currentSessionHours=
+currentSessionHours;
+
+
+
+return info;
+
+}
+
+
+
+std::string Device::getId() const
+
+{
+
+return id;
+
+}
+
+
+
+bool Device::getStatus() const
+
+{
+
+return isOn;
+
+}
