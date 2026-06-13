000979d4f8ab997c13da957407fdc02a5673db81
Vi <lamvi2692k6@gmail.com>
2026-06-13 13:08:34 +0700

    Update Device.h

--- SmartHomeProject/Backend/Device.h
+++ SmartHomeProject/Backend/Device.h
@@ -1,5 +1,112 @@
-#pragma once
-class Device {
+#ifndef DEVICE_H
+#define DEVICE_H
+
+
+#include <string>
+
+#include "../Data/DataStructures.h"
+
+
+class Device
+{
+
+
+protected:
+
+
+    std::string id;
+
+
+    std::string name;
+
+
+    std::string roomName;
+
+
+    std::string type;
+
+
+
+    bool isOn;
+
+
+
+    double currentPower;
+
+
+    double totalConsumption;
+
+
+    int extraParam;
+
+
+    double runningHours;
+
+
+    double currentSessionHours;
+
+
+
 public:
-    Device();
+
+
+    Device(
+        std::string id,
+        std::string name,
+        std::string roomName,
+        std::string type,
+        double power
+    );
+
+
+    virtual ~Device();
+
+
+
+    // bật thiết bị
+
+    virtual void turnOn();
+
+
+
+    // tắt thiết bị
+
+    virtual void turnOff();
+
+
+
+    // cập nhật điện năng
+
+    virtual void updateEnergy(
+        double deltaHours
+    );
+
+
+
+    // thay đổi tham số
+
+    virtual void updateParam(
+        int value
+    );
+
+
+
+    // trả dữ liệu cho UI
+
+    virtual DeviceInfo getInfo() const;
+
+
+
+    std::string getId() const;
+
+
+
+    bool getStatus() const;
+
+
+
 };
+
+
+
+#endif
